2c6b4bbba40d7f0f66faf41efe5552bce0565299
Steven Haryanto (on Asus Zenbook) <stevenharyanto@gmail.com>
2012-10-31 19:42:04 +0700

    First implementation, based on DZP::PodCoverageTests

---
+++ lib/Dist/Zilla/Plugin/Test/Rinci.pm
@@ -0,0 +1,55 @@
+package Dist::Zilla::Plugin::Test::Rinci;
+
+use Moose;
+extends 'Dist::Zilla::Plugin::InlineFiles';
+with 'Dist::Zilla::Role::PrereqSource';
+
+use namespace::autoclean;
+
+# VERSION
+
+# Register the release test prereq as a "develop requires"
+# so it will be listed in "dzil listdeps --author"
+sub register_prereqs {
+  my ($self) = @_;
+
+  $self->zilla->register_prereqs(
+    {
+      type  => 'requires',
+      phase => 'develop',
+    },
+    'Test::Rinci' => '0.01',
+  );
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
+# ABSTRACT: A release test for Rinci metadata
+
+=head1 DESCRIPTION
+
+This is an extension of L<Dist::Zilla::Plugin::InlineFiles>, providing the
+following files:
+
+  xt/release/rinci.t - a standard Rinci metadata test
+
+This test uses L<Test::Rinci> to check your Rinci metadata.
+
+
+=head1 ACKNOWLEDGEMENTS
+
+Code based on L<Dist::Zilla::Plugin::PodCoverageTests> by RJBS.
+
+=cut
+
+__DATA__
+___[ xt/release/rinci.t ]___
+#!perl
+
+use Test::More;
+
+eval "use Test::Rinci 0.01";
+plan skip_all => "Test::Rinci 0.01 required for testing Rinci metadata"
+  if $@;
+
+metadata_in_all_modules_ok();
